a07d29817ddbd10ef9edc8acc462bf01183b04c5
Idle <651462+reonZ@users.noreply.github.com>
2024-06-18 16:14:54 +0200

    document apps key shouldn't be restricted to number

--- src/types/foundry/client/data/documents/client-base-mixes.d.ts
+++ src/types/foundry/client/data/documents/client-base-mixes.d.ts
@@ -11776,7 +11776,7 @@ export class ClientBaseUser<TCharacter extends ClientBaseActor<null>> extends fo
      * Application in this object will have its render method called by {@link Document#render}.
      * @see {@link Document#render}
      */
-    apps: { [K in number]?: Application | ApplicationV2 };
+    apps: { [K in number | string]?: Application | ApplicationV2 };
 
     constructor(data: object, context?: DocumentConstructionContext<null>);
 
