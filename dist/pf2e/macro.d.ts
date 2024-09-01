e5721277925b687a2c2ab78e694c3076f8d0cf7e
Idle <651462+reonZ@users.noreply.github.com>
2024-09-01 11:52:40 +0200

    get rid of useless stuff

--- dist/pf2e/macro.d.ts
+++ dist/pf2e/macro.d.ts
@@ -1,9 +1,2 @@
 declare function resolveMacroActor(uuid?: ActorUUID): ActorPF2e | null;
-declare function openAttackpopup(actor: CharacterPF2e, { elementTrait, itemId, slug, type }: RollActionMacroParams, position?: ApplicationPosition): void;
-interface RollActionMacroParams {
-    itemId?: string;
-    slug?: string;
-    elementTrait?: ElementTrait;
-    type?: "blast" | "strike";
-}
-export { openAttackpopup, resolveMacroActor };
+export { resolveMacroActor };
